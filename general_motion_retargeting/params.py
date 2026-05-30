bad0903dd6b5a855e9d249ad948de501f3bb4772
Your Name <you@example.com>
2026-05-31 01:07:15 +0800

    feat: add vt_human_v2 configuration and update robot choices

--- general_motion_retargeting/params.py
+++ general_motion_retargeting/params.py
@@ -56,6 +56,7 @@ IK_CONFIG_DICT = {
         "stanford_toddy": IK_CONFIG_ROOT / "bvh_lafan1_to_toddy.json",
         "engineai_pm01": IK_CONFIG_ROOT / "bvh_lafan1_to_pm01.json",
         "pal_talos": IK_CONFIG_ROOT / "bvh_to_talos.json",
+        "vt_human_v2": IK_CONFIG_ROOT / "bvh_lafan1_to_vt_human_v2.json",
     },
     "bvh_nokov":{
         "unitree_g1": IK_CONFIG_ROOT / "bvh_nokov_to_g1.json",
